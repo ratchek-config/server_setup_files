d4317a9bc9ba4c352d88d3d5f913c407faee7226
ratchek <git@ratchek.com>
2023-04-27 20:17:49 -0400

    Split settings.ini into dev and production versions

--- settings.py
+++ settings.py
@@ -28,10 +28,11 @@ SECRET_KEY = config("DJANGO_SECRET_KEY", default=get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool) == True
 DEVELOPMENT_MODE = config("DEVELOPMENT_MODE", default=False, cast=bool) == True
-ALLOWED_HOSTS = [config("DOMAIN"), "www." + config("DOMAIN")]
 # Allow accessing site at localhost:8000 during development
 if DEVELOPMENT_MODE:
-    ALLOWED_HOSTS.append("localhost")
+    ALLOWED_HOSTS = ["localhost"]
+else:
+    ALLOWED_HOSTS = [config("DOMAIN"), "www." + config("DOMAIN")]
 
 # Application definition
 
